b78c70d4bec8cf49250525682eacbe0413311af9
Mohtawfik <mohammedtawfik51@gmail.com>
2025-01-30 00:21:42 +0100

    Updated backend to support PostgreSQL & MongoDB Cloud

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
+    ssl: isProduction ? { rejectUnauthorized: false } : false, // Enable SSL for cloud databases
 });
 
 module.exports = pool;
